a3defeb831b849c46a3592864526197faf2240ee
Cameron <github@camro.net>
2025-10-25 11:56:57 +1100

    Change assets path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('.'));
 
 // Serve assets folder publicly
-app.use(express.static("assets"));
+app.use("/assets", express.static("assets"));
 
 // Proxy endpoint for OpenAI API calls
 app.post('/api/generate-image', async (req, res) => {
